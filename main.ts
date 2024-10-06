9bd007812a513134f9c67c91b0b50d5c912a45da
Pedro Accarini Barros de Azevedo <122038856+P4dr4@users.noreply.github.com>
2024-10-06 01:14:26 -0300

    Development (#1)

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { context } from '@actions/github';
 
 export async function getOwner(): Promise<string> {
-    const owner = context.repo.owner;  // Access the owner from the repo context
+    const owner = context.repo.owner;
     console.log(owner);
     return owner;
 }
